d43457c36a7208f906cc8dc260befc2848d7b9a3
AleksandrPuhov <aleksandr_puhov@list.ru>
2020-03-31 00:57:38 +0300

    add swiper

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.testimonial-swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+})
